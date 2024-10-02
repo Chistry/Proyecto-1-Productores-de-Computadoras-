7c87b9309e4f5b215ca92e8da9baa3af5c71eef8
Chistry <christian.diaz@correo.unimet.edu.ve>
2024-10-02 00:10:27 -0400

    ClasesIniciales

---
+++ src/empleados/ProductoresFuentesAlimentacion.java
@@ -0,0 +1,23 @@
+package empleados;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author chris
+ */
+
+public class ProductoresFuentesAlimentacion extends Trabajador {
+    public ProductoresFuentesAlimentacion() {
+        super(16);  // Salario por hora
+    }
+
+    @Override
+    public void trabajar() {
+        // Lógica específica para producir placas base
+    }
+}
+
---
+++ src/empleados/ProductoresRAM.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package empleados;
+
+/**
+ *
+ * @author chris
+ */
+
+public class ProductoresRAM extends Trabajador {
+    public ProductoresRAM() {
+        super(40);  // Salario por hora
+    }
+
+    @Override
+    public void trabajar() {
+        // Lógica específica para producir placas base
+    }
+}
+
